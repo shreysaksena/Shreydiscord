a9f13067c9e714a181edb47dc6edc3ba1ae3d1ef
Shrey Saksena <shrey.saksena@gmail.com>
2022-07-02 03:26:09 +0530

    added features

--- main.py
+++ main.py
@@ -1,21 +1,30 @@
 import discord
+from discord.ext import commands
 import os
 import requests
 import json
 import random
+client = commands.Bot(command_prefix = '!')
+@client.event 
+async def on_ready():
+    print("I am ready {0.user}".format(client))
+@client.command()
+async def ping(ctx):
+    await ctx.send('Pong')
+@client.command()
+async def join(ctx):
+    channel = ctx.message.author.voice.channel
+    await channel.connect()  
 def get_quote():
     response = requests.get("https://zenquotes.io/api/random")
     json_data=json.loads(response.text)
     quote= json_data[0]['q']+ "- " + json_data[0]['a']
     return(quote)
-client = discord.Client()
 sad_words = ["sad","depressed","depressing","cry"]
 encourage = ["Cheer up!","Hang in there, you got this!","You are amazing! Don't be sad!"]
-@client.event 
-async def on_ready():
-    print("I am ready {0.user}".format(client))
 @client.event
 async def on_message(message):
+    await client.process_commands(message)
     msg = message.content
     if message.author == client.user:
         return
@@ -24,10 +33,9 @@ async def on_message(message):
     if message.content.startswith('$inspire'):
         quote=get_quote()
         await message.channel.send(quote)
-        channel = ctx.author.voice.channel
-        await channel.connect()
-    if any(word in msg for word in sad_words):
+    if any(word in msg.lower() for word in sad_words):
         await message.channel.send(random.choice(encourage))
+
 client.run(os.getenv('TOKEN'))
 
 
