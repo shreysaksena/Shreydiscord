87db9eee95169787e48e46f114f6a20becdb25f7
Shrey Saksena <shrey.saksena@gmail.com>
2022-07-10 23:29:26 +0530

    yo

--- main.py
+++ main.py
@@ -117,7 +117,9 @@ async def on_message(message):
     #     await message.channel.send("CS:GO is better game.")
 @client.command()
 async def chnick(ctx, member: discord.Member, nick):
+    await ctx.send('reached command')
     await member.edit(nick=nick)
+    await ctx.send('done')
 client.run(os.getenv('TOKEN'))
 
 
